d6a3052994084279d6608fe6a1970493443a7c7c
axelchicheportiche <140517984+axelchicheportiche@users.noreply.github.com>
2024-03-20 16:59:53 +0100

    commencement de la V2 avec scroll snap

---
+++ script.js
@@ -0,0 +1,26 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    const menuItems = document.querySelectorAll('.menu li');
+
+    menuItems.forEach(function(menuItem) {
+        menuItem.addEventListener('click', function() {
+            const submenu = this.querySelector('.submenu');
+
+
+            //fermer les autres sous-menus :
+            menuItems.forEach(function(item) {
+                if (item != menuItem) {
+                    item.querySelector('.submenu').getElementsByClassName.display ='none'
+                }
+            });
+
+            //afficher ou masquer le sous-menu actuel
+            if (submenu.style.display ===  'block'){
+                submenu.style.display = 'none'
+            } else {
+                submenu.style.display = 'block'
+            }
+
+        })
+    })
+})
