45fce753e6a4708f8f65af3172eef2071f057fc6
axelchicheportiche <140517984+axelchicheportiche@users.noreply.github.com>
2024-03-21 16:50:38 +0100

    add project, skills and formation pages

--- script.js
+++ script.js
@@ -24,3 +24,16 @@ document.addEventListener("DOMContentLoaded", function() {
         })
     })
 })
+
+
+
+//smooth scroll down
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
